4062b82da031cd83e788dd10a960d715eb4d3f3c
Nguyen Thai Binh <nguyenthaibinhbk@gmail.com>
2017-10-05 23:44:17 +0700

    add more favicon 6

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ g= [
 @app.route('/')
 def index():
     return render_template('index.html', girl_types = GirlType.objects())
-@app.route('/favico')
+@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico')
